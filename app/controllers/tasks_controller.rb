8e605ab7448abc9994fec3aa7eb9f1e2c5a98974
Anton Gordon <anton.gordon85@outlook.com>
2022-01-15 15:03:41 +0000

    read and started Create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,15 @@
 class TasksController < ApplicationController
     def index
+        @tasks = Task.all
     end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+    
 end
