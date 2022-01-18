e666364ad406293788a8f9e4a9c7ced81929b978
Anton Gordon <anton.gordon85@outlook.com>
2022-01-18 19:30:47 +0000

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+    before_action :find_task, only: [:show]
     def index
         @tasks = Task.all
     end
 
     def show
-        @tasks = Task[:id]
+       
     end
 
     def new
@@ -13,4 +14,9 @@ class TasksController < ApplicationController
     def create
     end
 
+    private
+
+    def find_task
+        @tasks = Task.find(params[:id])
+    end
 end
